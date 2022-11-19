16c8f59af368bfcc62efdd9d2caf566d432027c9
Ahmet ilhan <ailhan@hepsiemlak.com>
2022-11-19 16:27:50 +0300

    import bug fixed (.ts to .js)

--- dist/utils/index.d.ts
+++
@@ -1,1 +0,0 @@
-export declare const getRandomArbitrary: (min: number, max: number) => number;
--- dist/utils/index.js
+++
@@ -1,3 +0,0 @@
-export const getRandomArbitrary = (min, max) => {
-    return Math.random() * (max - min) + min;
-};
