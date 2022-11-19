30cf2c8f3e7f029b3ad55230fdb7e1bbe454ef55
Ahmet ilhan <ailhan@hepsiemlak.com>
2022-11-19 16:49:22 +0300

    create snow and push functions completed

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,7 +1,14 @@
 declare class SnowFall {
     constructor();
-    get cordinate(): number;
+    get cordinate(): {
+        left: number;
+        top: number;
+    };
+    get width(): number;
     get icon(): string;
+    get style(): string;
+    createContainer: () => HTMLDivElement;
+    get container(): HTMLDivElement;
     inner: () => void;
     makeItRain: () => void;
     init: () => void;
